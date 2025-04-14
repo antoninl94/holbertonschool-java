c7890e93ee8ba68b7a099b2e237f5c57d6080ed7
antoninl94 <9803@holbertonstudents.com>
2025-04-14 19:02:33 +0200

    Numbers0to99 modified all code

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -1,10 +1,11 @@
 public class Numbers0to99 {
   public static void main (String[] args) {
     for (int num = 0; num <= 99; num++) {
-      if (num == 99) {
-        System.out.printf("%d\n", num);
+      if (num > 0) {
+        System.out.print(", ");
       }
-      System.out.printf("%d, ", num);
+      System.out.printf("%d", num);
     }
+    System.out.print("\n");
   }
 }
