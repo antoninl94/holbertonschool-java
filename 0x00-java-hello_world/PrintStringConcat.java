20d070246c9dc0aa249009960dcf3ed3605e6fd1
antoninl94 <9803@holbertonstudents.com>
2025-04-14 17:18:12 +0200

    PrintStringConcat created

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,6 @@
+public class PrintStringConcat {
+  public static void main (String[] args) {
+    String text1 = "Holberton", text2 = "School", finalText = text1 + " " + text2;
+    System.out.printf("Welcome to %s !\n", finalText);
+  }
+}
