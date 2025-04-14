277ec2464edf56ff827e15fc232b9c374e4d1613
antoninl94 <9803@holbertonstudents.com>
2025-04-14 17:02:09 +0200

    PrintStringVariable created

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,7 @@
+public class PrintStringVariable {
+  public static void main (String[] args) {
+    String text = "Holberton School";
+    System.out.printf("%s\n%s\n", text, text);
+    System.out.println(text.split(" ")[0]);
+  }
+}
