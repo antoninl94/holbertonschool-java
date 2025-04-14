ebc042724367c3b58960af93fa9a2c9102ccaa6c
antoninl94 <9803@holbertonstudents.com>
2025-04-14 17:20:08 +0200

    PrintStringConcat hotfix

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,6 +1,8 @@
 public class PrintStringConcat {
   public static void main (String[] args) {
-    String text1 = "Holberton", text2 = "School", finalText = text1 + " " + text2;
+    String text1 = "Holberton";
+    String text2 = "School";
+    String finalText = text1 + " " + text2;
     System.out.printf("Welcome to %s !\n", finalText);
   }
 }
