9239fc5af847961ecd7518c0ca8a4d803f4c4d40
antoninl94 <9803@holbertonstudents.com>
2025-04-14 18:53:21 +0200

    Numbers0to99 created

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,10 @@
+public class Numbers0to99 {
+  public static void main (String[] args) {
+    for (int num = 0; num <= 99; num++) {
+      if (num == 99) {
+        System.out.println("99");
+      }
+      System.out.printf("%d, ", num);
+    }
+  }
+}
