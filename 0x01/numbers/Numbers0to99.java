e5642da55ff6cd8df6552db50b214215a2e5e52e
antoninl94 <9803@holbertonstudents.com>
2025-04-14 18:55:17 +0200

    Numbers0to99 modified print in condition

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -2,7 +2,7 @@ public class Numbers0to99 {
   public static void main (String[] args) {
     for (int num = 0; num <= 99; num++) {
       if (num == 99) {
-        System.out.println("99");
+        System.out.printf("%d\n", num);
       }
       System.out.printf("%d, ", num);
     }
